5474bfeca35ef8d07442ba97666c5123cd33863e
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-09-15 18:33:17 +0000

    coded

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 KestrelBryce All rights reserved
+// 
+// Created by KestrelBryce
+// Date: Sep. 15, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
